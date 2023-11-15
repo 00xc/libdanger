959e8502dc7b566467a2be9eea33d4deb451a6a1
Carlos López <48725664+00xc@users.noreply.github.com>
2023-11-15 16:21:30 +0100

    Remove needless whitespace

--- src/dngr_domain.c
+++ src/dngr_domain.c
@@ -15,7 +15,6 @@ DngrDomain* dngr_domain_new(void (*deallocator)(void*)) {
 }
 
 void dngr_domain_free(DngrDomain* dom) {
-
 	if (dom == NULL)
 		return;
 
@@ -58,25 +57,18 @@ uintptr_t dngr_load(DngrDomain* dom, const uintptr_t* prot_ptr) {
 }
 
 void dngr_drop(DngrDomain* dom, uintptr_t safe_val) {
-
 	if (!__dngr_list_remove(&dom->pointers, safe_val))
 		__builtin_unreachable();
 }
 
 static void __dngr_cleanup_ptr(DngrDomain* dom, uintptr_t ptr, int flags) {
-
 	if (!__dngr_list_contains(&dom->pointers, ptr)) {
-
 		/* We can deallocate straight away */
 		dom->deallocator((void*)ptr);
-
 	} else if (flags & DNGR_DEFER_DEALLOC) {
-
 		/* Defer deallocation for later */
 		__dngr_list_insert_or_append(&dom->retired, ptr);
-
 	} else {
-
 		/* Spin until all readers are done, then deallocate */
 		while (__dngr_list_contains(&dom->pointers, ptr));
 		dom->deallocator((void*)ptr);
@@ -89,7 +81,6 @@ void dngr_swap(DngrDomain* dom, uintptr_t* prot_ptr, uintptr_t new_val, int flag
 }
 
 int dngr_compare_and_swap(DngrDomain* dom, uintptr_t* prot_ptr, uintptr_t expected_val, uintptr_t new_val, int flags) {
-
 	if (!atomic_cas(prot_ptr, &expected_val, &new_val))
 		return 0;
 
@@ -102,25 +93,20 @@ void dngr_cleanup(DngrDomain* dom, int flags) {
 	uintptr_t ptr;
 
 	DNGR_LIST_ITER(&dom->retired, node) {
-
 		ptr = node->ptr;
 
 		if (ptr == 0)
 			continue;
 
 		if (!__dngr_list_contains(&dom->pointers, ptr)) {
-
 			/* We can deallocate straight away */
 			if (__dngr_list_remove(&dom->retired, ptr))
 				dom->deallocator((void*)ptr);
-
 		} else if (!(flags & DNGR_DEFER_DEALLOC)) {
-
 			/* Spin until all readers are done, then deallocate */
 			while (__dngr_list_contains(&dom->pointers, ptr));
 			if (__dngr_list_remove(&dom->retired, ptr))
 				dom->deallocator((void*)ptr);
 		}
-
 	}
 }
